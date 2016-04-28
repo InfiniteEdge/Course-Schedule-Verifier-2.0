6df3261a93cfb0bcf7ef87d843a305d0e36b09ed
Nick Karsten <nicholas.karsten@cuw.edu>
2016-04-28 13:10:00 -0500

    initial commit

---
+++ CourseScheduleVerifier2.0/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CourseScheduleVerifier2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Import = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(27, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(580, 329);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(300, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Conflicting Classes";
+            // 
+            // Import
+            // 
+            this.Import.Location = new System.Drawing.Point(181, 451);
+            this.Import.Name = "Import";
+            this.Import.Size = new System.Drawing.Size(75, 23);
+            this.Import.TabIndex = 2;
+            this.Import.Text = "Import...";
+            this.Import.UseVisualStyleBackColor = true;
+            this.Import.Click += new System.EventHandler(this.Import_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 451);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 571);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Import);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Import;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CourseScheduleVerifier2.0/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Packaging;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+
+
+
+namespace CourseScheduleVerifier2._0
+{
+    public partial class Form1 : Form
+    {
+        public struct courseDetails
+        {
+
+
+            public string instructor;
+            public string room;
+            public string day;
+            public string time;
+
+        }
+
+        //keeps track of how many classes were added 
+        int classCounter = 0;
+
+        //Stores courseDetails structs in a list
+        public List<courseDetails> courseList = new List<courseDetails>();
+
+       
+        char currColumn;
+        int currRow;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Import_Click(object sender, EventArgs e)
+        {
+            //create instance of OpenFileDialog box
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //Filter to excel files only
+            ofd.Filter = "Excel Files (.xlsx)|*.xlsx|All Files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                string filePath = ofd.FileName;
+                
+                //Loops through each column and row containing pertinent data to find conflicting classes
+                for (int i = 4; i <= 13; i++)
+                {
+                    currRow = i;
+                    if (GetCellValues(filePath, "Sheet1", ("B" + currRow).ToString()) == "CSC" || (GetCellValues(filePath, "Sheet1", ("B" + currRow).ToString()) == "SCI"))
+                    {
+
+
+                        courseDetails courseDetails1;
+
+
+                        courseDetails1.instructor = ((GetCellValues(filePath, "Sheet1", "G" + currRow)) + (GetCellValues(filePath, "Sheet1", "H" + currRow)));
+                        courseDetails1.room = ((GetCellValues(filePath, "Sheet1", "K" + currRow)) + (GetCellValues(filePath, "Sheet1", "L" + currRow)));
+                        courseDetails1.day = ((GetCellValues(filePath, "Sheet1", "O" + currRow)) + (GetCellValues(filePath, "Sheet1", "P" + currRow)) + (GetCellValues(filePath, "Sheet1", "Q" + currRow)) + (GetCellValues(filePath, "Sheet1", "R" + currRow)) + (GetCellValues(filePath, "Sheet1", "S" + currRow)));
+                        courseDetails1.time = ((GetCellValues(filePath, "Sheet1", "M" + currRow)) + (GetCellValues(filePath, "Sheet1", "N" + currRow)));
+
+                        courseList.Add(courseDetails1);                       
+                        
+                    }
+                }
+
+                for (int j = 0; j < courseList.Count; j++)
+                {
+                    listBox1.Items.Add(courseList[j].instructor + courseList[j].room + courseList[j].day + courseList[j].time);
+
+                }
+            }
+        }
+
+        public static string GetCellValues(string fileName, string sheetName, string addressName)
+        {
+            string value = null;
+
+            using (SpreadsheetDocument document = SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart wbPart = document.WorkbookPart;
+                Sheet theSheet = wbPart.Workbook.Descendants<Sheet>().
+                    Where(s => s.Name == sheetName).FirstOrDefault();
+
+                if (theSheet == null)
+                {
+                    throw new ArgumentException("sheetName");
+                }
+
+                WorksheetPart wsPart = (WorksheetPart)(wbPart.GetPartById(theSheet.Id));
+                Cell theCell = wsPart.Worksheet.Descendants<Cell>().
+                    Where(c => c.CellReference == addressName).FirstOrDefault();
+
+                if (theCell != null)
+                {
+                    value = theCell.InnerText;
+                }
+
+                if (theCell.DataType != null)
+                {
+                    switch (theCell.DataType.Value)
+                    {
+                        case CellValues.SharedString:
+
+                            // For shared strings, look up the value in the
+                            // shared strings table.
+                            var stringTable =
+                                wbPart.GetPartsOfType<SharedStringTablePart>()
+                                .FirstOrDefault();
+
+                            // If the shared string table is missing, something 
+                            // is wrong. Return the index that is in
+                            // the cell. Otherwise, look up the correct text in 
+                            // the table.
+                            if (stringTable != null)
+                            {
+                                value =
+                                    stringTable.SharedStringTable
+                                    .ElementAt(int.Parse(value)).InnerText;
+                            }
+                            break;
+
+                        case CellValues.Boolean:
+                            switch (value)
+                            {
+                                case "0":
+                                    value = "FALSE";
+                                    break;
+                                default:
+                                    value = "TRUE";
+                                    break;
+                            }
+                            break;
+                    }
+
+                }
+            }
+            return value;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
